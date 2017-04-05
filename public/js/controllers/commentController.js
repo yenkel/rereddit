91040b72a15369711b4d87beecee623d5dbc3c22
Steven Winston <swinston100@hotmail.com>
2017-04-05 11:56:35 +0300

    Update commentController.js

--- public/js/controllers/commentController.js
+++ public/js/controllers/commentController.js
@@ -1,15 +1,5 @@
 app.controller('CommentController', function($scope, $stateParams, postFactory) {
 
-  //todo - this needs to be a function that retrieves a single
-  //post and it's comments from the server based on the post ID
-  postFactory.getPost($stateParams.id)
-    .then(function(results) {
-      //todo
-    })
-    .catch(function(error) {
-      //todo
-    });
-
   $scope.addComment = function() {
     //todo
   }
