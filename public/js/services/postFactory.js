edeb3021af13cd4a8813307436bf8e7a7e1e71d1
Steven Winston <swinston100@hotmail.com>
2017-04-05 10:46:38 +0300

    Update postFactory.js

--- public/js/services/postFactory.js
+++ public/js/services/postFactory.js
@@ -2,16 +2,14 @@ app.factory('postFactory', function() {
 
   var posts = {
     //todo
+    //add post
+    //up/down vote post
+    //get posts
+    //get post (and it's comments)
+    //add comment (to post)
+    //up/down vote comment (belonging to post)
+    //extension: admin can delete post
+    //extension: admin can delete comment (from post)
   }
   return posts;
-
-  //add post
-  //up/down vote post
-  //get posts
-  //get post (and it's comments)
-  //add comment (to post)
-  //up/down vote comment (belonging to post)
-  //extension: admin can delete post
-  //extension: admin can delete comment (from post)
-
 });
