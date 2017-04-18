98d4ec34c9ac6988f22c7a070430278c9635420d
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-04-18 10:56:30 +0300

    image

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,11 +7,11 @@ var passport = require('../models/passport');
 router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 router.get('/facebook/callback',
-passport.authenticate('facebook', { session:false,failureRedirect: '/' }),
-function(req, res) {
-  res.redirect('/authorization?token=' + req.user.token + "&name=" + req.user.name);
-  // Successful authentication, redirect home.
-  res.redirect('/');
-});
+    passport.authenticate('facebook', { session: false, failureRedirect: '/' }),
+    function(req, res) {
+        res.redirect('/authorization?token=' + req.user.token + "&name=" + req.user.name);
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
 
 module.exports = router;
